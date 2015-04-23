ec19d9dd5e54f290337733eb58b3febfa671ff11
Howon Lee <howon@howonlee.com>
2015-04-23 02:05:53 -0700

    can cohere now, although no plotting

--- analysis.py
+++ analysis.py
@@ -7,6 +7,7 @@ import itertools
 import operator
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+import matplotlib.mlab as mat_mlab
 import numpy.fft as np_f
 import numpy.linalg as np_l
 import numpy as np
